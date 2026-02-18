f3141ee008c95a571d033725647f1b6a20991abd
Vanshita Pahwa <pahwavanshita@gmail.com>
2026-02-18 10:22:06 +0530

    baby

--- classesandobjects.cpp
+++ classesandobjects.cpp
@@ -1,2 +1,21 @@
 #include<iostream>
 using namespace std;
+class triangle{
+    public:
+    int base;
+    int height;
+    void area(){
+        cout<<"enter base and height of triangle: ";
+        cin>>base>>height;
+    }
+    double area1(){
+        return 0.5*base*height;
+    }
+};
+
+int main(){
+    triangle t1;
+    t1.area();
+    cout<<"area of triangle is: "<<t1.area1()<< endl;
+    return 0;
+}
