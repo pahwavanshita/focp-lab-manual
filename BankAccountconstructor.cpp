1c91de0f461a1e12924ea42f1e7bc0b6faa6a2d6
Vanshita Pahwa <pahwavanshita@gmail.com>
2026-03-10 12:46:43 +0530

    baby

---
+++ BankAccountconstructor.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name;
+    int acc_no;
+    float balance;
+    cout<<"Enter your name: ";
+    cin>>name;
+    cout<<"Enter your account number: ";
+    cin>>acc_no;
+    cout<<"Enter your balance: ";
+    cin>>balance;
+    cout<<"Name: "<<name<<endl;
+    cout<<"Account Number: "<<acc_no<<endl;
+    cout<<"Balance: "<<balance<<endl;
+    return 0;
+}
